9ccf816aac2287526e146fee3c25ec8edbdcb906
Janos85 <janosbalogh85@gmail.com>
2024-06-05 15:37:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# checkliste0605
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/checkliste0605/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.checkliste0605
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
